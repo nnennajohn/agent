12174d7e323336f672c5061330d4d886223025f5
Ian Macartney <ianhmacartney@gmail.com>
2025-07-15 14:18:32 -0700

    setup feedback

--- setup.cjs
+++ setup.cjs
@@ -4,13 +4,22 @@
 const { join } = require("path");
 const { execSync } = require("child_process");
 
+console.log("Installing dependencies for the Agent component...");
 execSync("npm install", { cwd: __dirname, stdio: "inherit" });
+console.log("✅\n");
+console.log("Building the Agent component...");
 execSync("npm run build", { cwd: __dirname, stdio: "inherit" });
+console.log("✅\n");
+console.log("Installing dependencies for the playground...");
 execSync("npm install", {
   cwd: join(__dirname, "./playground"),
   stdio: "inherit",
 });
+console.log("✅\n");
+console.log("Installing dependencies for the example...");
 execSync("npm install", {
   cwd: join(__dirname, "./example"),
   stdio: "inherit",
 });
+console.log("✅\n");
+console.log("Now run: npm run dev");
